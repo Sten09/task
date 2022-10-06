1edb7781b4bb7cfeff13cb5bd5aab6cea29b5e41
Sten09 <sten111982@gmail.com9>
2022-10-06 10:39:37 +0300

    transh

---
+++ Задача 2/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+a = 5; b = 7 -> max = 7
+a = 2 b = 10 -> max = 10
+a = -9 b = -3 -> max = -3
+*/
+
+Console.WriteLine("Введите число A ");
+string A = Console.ReadLine();
+Console.WriteLine("Введите число B ");
+string B = Console.ReadLine();
+
+int numA = int.Parse(A);
+int numB = int.Parse(B);
+
+if(numA > numB)
+Console.WriteLine($"{numA} = максимальное число");
+else
+Console.WriteLine($"Максимальное число = {numB}");
+Console.WriteLine($"Минимальное число = {numA}");
---
+++ Задача 4/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+
+*/
+
+Console.WriteLine("Введите первое число");
+string number1 = Console.ReadLine();
+
+Console.WriteLine("Введите второе число");
+string number2 = Console.ReadLine();
+
+Console.WriteLine("Ведите третье число");
+string number3 = Console.ReadLine();
+
+int num1 = int.Parse(number1);
+int num2 = int.Parse(number2);
+int num3 = int.Parse(number3);
+int max = num1;
+
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+
+Console.WriteLine($"Максимальное число = {max}");
